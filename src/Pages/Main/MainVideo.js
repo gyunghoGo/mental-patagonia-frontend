35f5748d60f2d36ea7e8b4451eb3930082e87948
rimuuu <soryonew@gmail.com>
2020-06-09 20:01:47 +0900

    [Add] 메인페이지 슬라이더 구현 및 mock data 활용하여 fetching 적용 - 카테고리 이미지 슬라이더 ui 1차 완료 - 버튼 클릭 시 이미지 트랜지션 적용 - hover 애니메이션 추가 적용 예정 - mock data & fetching 활용 - 테스트용 코드 삭제

--- src/Pages/Main/MainVideo.js
+++ src/Pages/Main/MainVideo.js
@@ -5,21 +5,65 @@ import hero_video from "../../images/video/hero_video.mp4";
 
 const MainVideo = () => {
   return (
-    <StyledMainVideo>
+    <MainVideoWrap>
       <video playsinline autoplay="true" muted loop>
         <source src={hero_video} type="video/mp4" />
       </video>
-    </StyledMainVideo>
+      <ContentWrap>
+        <HeroImg
+          src="https://www.patagonia.com/on/demandware.static/-/Library-Sites-PatagoniaShared/default/dw22352c4b/images/svg/D15-Web-Wordmark-750px-01-v2.svg"
+          alt="hero"
+        />
+        <ContentP>
+          Justice for the most polluted neighborhood in Los Angeles.
+        </ContentP>
+        <Button>Watch</Button>
+      </ContentWrap>
+    </MainVideoWrap>
   );
 };
 
 export default MainVideo;
 
-const StyledMainVideo = styled.div`
+const MainVideoWrap = styled.section`
   position: relative;
   width: 100vw;
   height: 100vh;
   z-index: -100;
   background-size: cover;
   overflow: hidden;
+`;
+
+const ContentWrap = styled.div`
+  position: absolute;
+  width: 100%;
+  top: 40%;
+  margin: 0 auto;
+  text-align: center;
+  color: #ffff;
+`;
+
+const HeroImg = styled.img`
+  width: 400px;
+`;
+
+const ContentP = styled.p`
+  font-size: 20px;
+  font-weight: 600;
+  margin: 40px 0;
+`;
+
+const Button = styled.button`
+  padding: 0.8rem 3rem;
+  border-radius: 3rem;
+  border: 0.3rem solid #ffffff;
+  background-color: transparent;
+  margin-right: 2rem;
+  background-color: #ffff;
+  font-weight: 700;
+  font-size: 1rem;
+  transition: 0.2s;
+  &:hover {
+    transform: scale(1.05);
+  }
 `;
---
+++ src/Pages/Main/SlideCarousel.js
@@ -0,0 +1,96 @@
+import React, { useState, useEffect, useRef } from "react";
+import styled, { css } from "styled-components";
+import Slide from "./Slide";
+
+function SlideCarousel() {
+  const [categoryData, setCategoryData] = useState([]);
+
+  useEffect(() => {
+    fetch("/data/data.json")
+      .then((res) => res.json())
+      .then((res) => setCategoryData(res.category));
+  }, []);
+
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [positionX, setPositionX] = useState(0);
+
+  const nextSlide = () => {
+    let TOTAL_SLIDES = categoryData.length;
+    if (currentSlide >= TOTAL_SLIDES - 3) {
+      return;
+    } else {
+      setCurrentSlide(currentSlide + 1);
+      setPositionX(positionX - 300);
+    }
+  };
+
+  const prevSlide = () => {
+    if (currentSlide === 0) {
+      return;
+    } else {
+      setCurrentSlide(currentSlide - 1);
+      setPositionX(positionX + 300);
+    }
+  };
+
+  return (
+    <Container>
+      <SliderContainer
+        style={{
+          transform: `translateX(${positionX}px)`,
+          transition: "all 0.3s ease-in-out",
+        }}
+      >
+        <Slide data={categoryData} />
+      </SliderContainer>
+      <BtnWrap>
+        <Button className="prev" onClick={prevSlide}>
+          <i class="fas fa-chevron-left"></i>
+        </Button>
+        <Button className="next" onClick={nextSlide}>
+          <i class="fas fa-chevron-right"></i>
+        </Button>
+      </BtnWrap>
+    </Container>
+  );
+}
+
+export default SlideCarousel;
+
+const Container = styled.section`
+  overflow: hidden;
+  width: 100vw;
+  height: 50vh;
+  margin: 0 auto;
+  position: relative;
+  padding: 0 5%;
+`;
+
+const SliderContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+`;
+
+const BtnWrap = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 92%;
+  position: absolute;
+  top: 42%;
+`;
+
+const Button = styled.button`
+  width: 60px;
+  height: 60px;
+  font-size: 20px;
+  color: black;
+  border-radius: 50%;
+  z-index: 9;
+  box-shadow: 0 6px 8px 0 rgba(0, 0, 0, 0.2);
+  &:hover {
+    transition: all 0.4s ease-in-out;
+    transform: scale(1.1);
+  }
+`;
